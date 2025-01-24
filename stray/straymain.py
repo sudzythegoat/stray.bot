34b746cb39672f6f1f70f3fc86f4965d39256d26
Sudzy <167475761+sudzythegoat@users.noreply.github.com>
2025-01-24 07:35:24 -0600

    Update straymain.py

--- stray/straymain.py
+++ stray/straymain.py
@@ -22,4 +22,38 @@ async def ban(ctx, member: discord.Member, *, reason=None):
     except discord.HTTPException:
         await ctx.send("An error occurred while trying to ban this user.")
 
-bot.run('YOUR_BOT_TOKEN')
+@bot.command()
+@commands.has_permissions(kick_members=True)
+async def kick(ctx, member: discord.Member, *, reason=None):
+    try:
+        await member.kick(reason=reason)
+        await ctx.send(f'{member} has been kicked for: {reason}')
+    except discord.Forbidden:
+        await ctx.send("I don't have permission to kick this user.")
+    except discord.HTTPException:
+        await ctx.send("An error occurred while trying to kick this user.")
+
+@bot.command()
+@commands.has_permissions(moderate_members=True)
+async def mute(ctx, member: discord.Member, duration: int, *, reason=None):
+    try:
+        timeout_until = discord.utils.utcnow() + discord.timedelta(minutes=duration)
+        await member.edit(timeout_until=timeout_until, reason=reason)
+        await ctx.send(f'{member} has been muted out for {duration} minutes. Reason: {reason}')
+    except discord.Forbidden:
+        await ctx.send("I don't have permission to mute this user.")
+    except discord.HTTPException:
+        await ctx.send("An error occurred while trying to mute this user.")
+        
+@bot.command()
+@commands.has_permissions(moderate_members=True)
+async def delmute(ctx, member: discord.Member):
+    try:
+        await member.edit(timeout_until=None)
+        await ctx.send(f'{member} has been unmuted.')
+    except discord.Forbidden:
+        await ctx.send("I don't have permission to unmute this user.")
+    except discord.HTTPException:
+        await ctx.send(f"An error occurred while trying to unmute {member}.")
+
+bot.run(token)
