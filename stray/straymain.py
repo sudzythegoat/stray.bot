66e5ceb33d0c5351f8f071aa3cdc319cdf4c6660
Sudzy <167475761+sudzythegoat@users.noreply.github.com>
2025-01-24 07:29:31 -0600

    Update straymain.py

--- stray/straymain.py
+++ stray/straymain.py
@@ -1,9 +1,25 @@
 import discord
 from discord.ext import commands
-import requests
-import json
-bot = commands.Bot(command_prefix="?")
-@bot.command()
 
-async def ban(ctx, user):
-  
+intents = discord.Intents.default()
+intents.messages = True
+intents.guilds = True
+intents.members = True
+bot = commands.Bot(command_prefix='?', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}!')
+
+@bot.command()
+@commands.has_permissions(ban_members=True)
+async def ban(ctx, member: discord.Member, *, reason=None):
+    try:
+        await member.ban(reason=reason)
+        await ctx.send(f'{member} has been banned for: {reason}')
+    except discord.Forbidden:
+        await ctx.send("I don't have permission to ban this user.")
+    except discord.HTTPException:
+        await ctx.send("An error occurred while trying to ban this user.")
+
+bot.run('YOUR_BOT_TOKEN')
